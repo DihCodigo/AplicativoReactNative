c4390d07c90bc4731cd2d830304e707c630bf722
César Diego Anovich <iddiegobis@gmail.com>
2024-09-13 14:00:46 -0300

    Aplicativo add

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  function BotaoLogar(){
+    alert('Logado com sucesso')
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerTop}>
+      <View>
+        <Text style={styles.titlePrincipal}>Objective Homolog</Text>
+      </View>
+      <View style={styles.container} >
+        <Text style={styles.title}>Login</Text>
+        <TextInput 
+          style={styles.textInput} 
+          placeholder="E-mail" 
+          keyboardType="email-address" 
+          autoCapitalize="none" 
+        />
+        <TextInput 
+          style={styles.textInput} 
+          placeholder="Senha" 
+          secureTextEntry 
+        />
+        <Button style={styles.butao} title="Logar" onPress={() => alert('Logando...')} />
+        <TouchableOpacity onPress={() => alert('Redefinir senha...')}>
+          <Text style={styles.resetText}>Esqueceu a senha? Redefinir</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -16,5 +41,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    width: '100%'
   },
+  containerTop: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    width: '100%'
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    backgroundColor: '#5f6163',
+    width: '100%',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 15,
+    color: '#fff',
+  },
+  resetText: {
+    color: '#007BFF',
+    marginTop: 10,
+    textDecorationLine: 'underline',
+  },
+  titlePrincipal: {
+    color: '#911603',
+    marginTop: 40,
+    fontSize: 30
+  },
+  butao: {
+    marginTop: 50,
+    width: '100%'
+  }
 });
